7e5fa10db3dea64cd0e9b3b880a319ca2bbdfd71
mikefusc <mikefusc@me.com>
2017-09-05 23:08:44 -0400

    Update waivers.js

--- waivers.js
+++ waivers.js
@@ -1,6 +1,6 @@
-if($("#waivediv").text().match([1-9])){
-    $("#waivediv").show();
-    $("#waivediv").effect( "pulsate", {times:5}, 5000 );
+if($("#tradediv").text().match([1-9])){
+    $("#tradediv").show();
+    $("#tradediv").effect( "pulsate", {times:5}, 5000 );
 } else {
-    $("#waivediv").hide();
+    $("#tradediv").hide();
 }
