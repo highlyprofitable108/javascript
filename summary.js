42708676df9688a2ef80e2a92f2fab49ef7e6666
mikefusc <mikefusc@me.com>
2017-09-06 19:45:19 -0400

    Update summary.js

--- summary.js
+++ summary.js
@@ -1,4 +1,3 @@
 $("#summarydiv").click(function(){ 
-    event.preventDefault(); 
-    window.location.href = "http://www68.myfantasyleague.com/2017/home/48963?MODULE=MESSAGE13";
+    event.preventDefault();
 });
