f181055f1142d8d3441466521aa1ae6ad052c3ee
mikefusc <mikefusc@me.com>
2017-09-06 11:07:11 -0400

    Update scripting.js

--- scripting.js
+++ scripting.js
@@ -4,16 +4,23 @@ if(today.getDay() == 0 || today.getDay() == 1) {
 } else {
     $("#articlediv").show();
 }
-if($("#waivediv").text().match([1])){
-    $("#waivediv").show();
-    $("#waivediv").effect( "pulsate", {times:5}, 5000 );
-} else {
-    $("#waivediv").hide();
-}
 if(today.getDay() == 3 || today.getDay() == 4 || today.getDay() == 5) {
     $("#matchdiv").show();
     $("#scorediv").hide();
 } else {
     $("#scorediv").show();
     $("#matchdiv").hide();
+}
+if($("#tradediv").text().match([1-9])) {
+    $("#tradediv").show();
+    $("#tradediv").effect( "pulsate", {times:5}, 5000 );
+} else {
+    $("#tradediv").hide();
+}
+if($("#waivediv").text().match([1])){
+    $("#waivediv").show();    
+    $("#waivediv").effect( "pulsate", {times:5}, 5000 );
+} 
+else {    
+    $("#waivediv").hide();
 }
