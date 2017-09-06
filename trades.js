e7af428a802f263dcc50d5c2d500da825b128dea
mikefusc <mikefusc@me.com>
2017-09-06 00:28:43 -0400

    Update trades.js

--- trades.js
+++ trades.js
@@ -1,6 +1,2 @@
-if($("#tradediv").text().match([1-9])) {
-    $("#tradediv").show();
-    $("#tradediv").effect( "pulsate", {times:5}, 5000 );
-} else {
-    $("#tradediv").hide();
-}
+$("#matchdiv").hide();
+$("#scorediv").hide();
