e44aca632437dd865054316cb14db96c7828b25d
mikefusc <mikefusc@me.com>
2017-09-06 00:13:01 -0400

    Update head2head.js

--- head2head.js
+++ head2head.js
@@ -1,5 +1,5 @@
 var today = new Date();
-if(today.getDay() > 2 || today.getDay() < 4) {
+if(today.getDay() == 2 || today.getDay() == 3 || today.getDay() == 4) {
     $("#matchdiv").show();
     $("#scorediv").hide();
 } else {
