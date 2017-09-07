87984fc0f25ec23b42001954502e6bdddc29bdeb
mikefusc <mikefusc@me.com>
2017-09-06 22:59:16 -0400

    Update article.js

--- article.js
+++ article.js
@@ -3,12 +3,18 @@ if(today.getDay() == 0 || today.getDay() == 1) {
     $("#articlediv").hide();
     $("#standdiv").hide();
     $("#stand2div").hide();
+    $("#briefdiv").show();
+    $("#maindiv").show();
 } else if(today.getDay() == 2) {
     $("#articlediv").show();
     $("#standdiv").show();
     $("#stand2div").show();
+    $("#briefdiv").hide();
+    $("#maindiv").hide();
 } else {
     $("#articlediv").show();
     $("#standdiv").hide();
     $("#stand2div").hide();
+    $("#briefdiv").show();
+    $("#maindiv").show();
 }
