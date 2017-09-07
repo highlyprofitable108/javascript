4b4d710d01b1eab333f469e42c93d46df91b1934
mikefusc <mikefusc@me.com>
2017-09-06 20:55:35 -0400

    Update waive2.js

--- waive2.js
+++ waive2.js
@@ -4,3 +4,7 @@ if($("#waive2div").text().match([1])){
 } else {
     $("#waive2div").hide();
 }
+$("#waive2div").click(function(){ 
+    event.preventDefault();
+    window.location.href = "http://www68.myfantasyleague.com/2017/add_drop?L=48963";
+});
