d8bc9833690980ca19951f01d9cf0a474d125186
mikefusc <mikefusc@me.com>
2017-09-06 21:56:18 -0400

    Update head2head.js

--- head2head.js
+++ head2head.js
@@ -18,4 +18,4 @@ if(today.getDay() == 3 || today.getDay() == 4) {
 function removeElement(ele) {
     ele.parentNode.removeChild(ele);
 }
-$('img[src="http://www.nitrografixx.com/MFL-Popups/alert-icon-orange.png"]').remove();
+$('img[src="//www.nitrografixx.com/MFL-Popups/alert-icon-orange.png"]').remove();
