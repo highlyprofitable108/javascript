78f810ea28cb270b23be3ce96d05f863b68d9fa9
mikefusc <mikefusc@me.com>
2017-09-06 22:53:55 -0400

    Update article.js

--- article.js
+++ article.js
@@ -1,6 +1,14 @@
 var today = new Date();
 if(today.getDay() == 0 || today.getDay() == 1) {
     $("#articlediv").hide();
+    $("#standdiv").hide();
+    $("#stand2div").hide();
+} else if(today.getDay() == 2) {
+    $("#articlediv").show();
+    $("#standdiv").show();
+    $("#stand2div").show();
 } else {
     $("#articlediv").show();
+    $("#standdiv").hide();
+    $("#stand2div").hide();
 }
