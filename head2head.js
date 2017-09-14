8586fbab141d9c9873515180c02f9193a4c5a3ff
mikefusc <mikefusc@me.com>
2017-09-14 12:02:19 -0400

    Update head2head.js

--- head2head.js
+++ head2head.js
@@ -2,21 +2,25 @@ var today = new Date();
 if(today.getDay() == 3) {
     $("#matchdiv").show();
     $("#scorediv").hide();
+    $("#score2div").hide();
     $("#lastdiv").show();
     $("#last2div").hide();
 } else if(today.getDay() == 2) {
     $("#matchdiv").show();
     $("#scorediv").hide();
+    $("#score2div").hide();
     $("#lastdiv").hide();
     $("#last2div").show();
 } else if(today.getDay() == 4) {
     $("#matchdiv").show();
     $("#scorediv").show();
+    $("#score2div").show();
     $("#lastdiv").hide();
     $("#last2div").hide();
 } else {
     $("#matchdiv").hide();
     $("#scorediv").show();
+    $("#score2div").show();
     $("#lastdiv").hide();
     $("#last2div").hide();
 }
