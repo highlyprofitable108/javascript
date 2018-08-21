531c6741591810601376ab69f97a9df3f3906dc2
mikefusc <mikefusc@me.com>
2018-08-20 22:50:59 -0400

    Update summary.js

--- summary.js
+++ summary.js
@@ -1,8 +1,8 @@
 $("#summarydiv").click(function(){ 
     event.preventDefault();
-    window.location.href = "http://www68.myfantasyleague.com/2017/home/48963?MODULE=MESSAGE13";
+    window.location.href = "http://www68.myfantasyleague.com/2018/home/48963?MODULE=MESSAGE13";
 });
 $("#summary2div").click(function(){     
         event.preventDefault();    
-        window.location.href = "http://www68.myfantasyleague.com/2017/home/48963?MODULE=MESSAGE13";
+        window.location.href = "http://www68.myfantasyleague.com/2018/home/48963?MODULE=MESSAGE13";
 });
