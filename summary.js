c93caa3c18b30cdac1317c24089ceb1bfc84704b
mikefusc <mikefusc@me.com>
2020-01-05 11:56:26 -0500

    Update summary.js

--- summary.js
+++ summary.js
@@ -1,8 +1,8 @@
 $("#summarydiv").click(function(){ 
     event.preventDefault();
-    window.location.href = "http://www68.myfantasyleague.com/2019/home/48963?MODULE=MESSAGE13";
+    window.location.href = "http://www68.myfantasyleague.com/2019/home/32505?MODULE=MESSAGE13";
 });
 $("#summary2div").click(function(){     
         event.preventDefault();    
-        window.location.href = "http://www68.myfantasyleague.com/2019/home/48963?MODULE=MESSAGE13";
+        window.location.href = "http://www68.myfantasyleague.com/2019/home/32505?MODULE=MESSAGE13";
 });
