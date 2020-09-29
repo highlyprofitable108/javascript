da27b0535963bf1c1c8c531b71902d8c48847cfc
mikefusc <mikefusc@me.com>
2020-09-28 21:25:56 -0400

    Update summary.js

--- summary.js
+++ summary.js
@@ -1,8 +1,8 @@
 $("#summarydiv").click(function(){ 
     event.preventDefault();
-    window.location.href = "http://www68.myfantasyleague.com/2020/home/48963?MODULE=MESSAGE13";
+    window.location.href = "https://www68.myfantasyleague.com/2020/ajax_ls?L=48963";
 });
 $("#summary2div").click(function(){     
         event.preventDefault();    
-        window.location.href = "http://www68.myfantasyleague.com/2020/home/48963?MODULE=MESSAGE13";
+        window.location.href = "https://www68.myfantasyleague.com/2020/ajax_ls?L=48963";
 });
