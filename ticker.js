1b486c85121bc34e96c8e9ac7cb3985f145621d3
mikefusc <mikefusc@me.com>
2021-09-11 22:34:10 -0400

    Create ticker.js

---
+++ ticker.js
@@ -0,0 +1,9 @@
+if($("#waivediv").text().match([1])){
+
+} else {
+    if($("#tradediv").text().match(1) || $("#tradediv").text().match(2) || $("#tradediv").text().match(3) || $("#tradediv").text().match(4) || $("#tradediv").text().match(5)) {
+
+    } else {
+        $("#tickerdiv").hide();
+    }
+});
