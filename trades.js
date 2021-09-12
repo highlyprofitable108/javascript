d89ce85f3806e990a60b91e749fe2933fc0da4df
mikefusc <mikefusc@me.com>
2021-09-11 23:41:47 -0400

    Update trades.js

--- trades.js
+++ trades.js
@@ -4,16 +4,3 @@ if($("#tradediv").text().match(1) || $("#tradediv").text().match(2) || $("#trade
 } else {
     $("#tradediv").hide();
 }
-if($("#waivediv").text().match([1])){
-    $("#waivediv").show();
-    $("#waivediv").effect( "pulsate", {times:5}, 5000 );
-} else {
-    $("#waivediv").hide();
-}
-if(window.location.href.indexOf("add_drop") > -1) {
-    alert("Alert: Desktop!");
-    $("#waivediv").hide();
-} else {
-    alert("Alert: NOPE!");
-}
-
