7922089fda8d9efd53bf62c053075964468df174
mikefusc <mikefusc@me.com>
2021-09-11 23:21:47 -0400

    Update waive.js

--- waive.js
+++ waive.js
@@ -4,6 +4,6 @@ if($("#waivediv").text().match([1])){
 } else {
     $("#waivediv").hide();
 }
-if($("container-wrap").text().match("CURRENT WAIVER CLAIMS"){
+if($("container-wrap").text().match("Current Waiver Claims"){
    $("#waivediv").hide();
 }
