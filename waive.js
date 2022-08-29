c20ba71be2800e85cd5baebb5795287f209aea2d
mikefusc <mikefusc@me.com>
2022-08-29 18:18:16 -0400

    Update waive.js

--- waive.js
+++ waive.js
@@ -7,5 +7,5 @@ if(window.location.href.indexOf("add_drop") > -1) {
 }
 $("#waivediv").click(function(){ 
     event.preventDefault();
-    window.location.href = "http://www68.myfantasyleague.com/2021/add_drop?L=48963";
+    window.location.href = "http://www68.myfantasyleague.com/2022/add_drop?L=48963";
 });
