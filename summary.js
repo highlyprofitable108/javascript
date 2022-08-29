52941e8d92d53cd42dce2c1061e2489533616f3a
mikefusc <mikefusc@me.com>
2022-08-29 18:17:55 -0400

    Update summary.js

--- summary.js
+++ summary.js
@@ -1,8 +1,8 @@
 $("#summarydiv").click(function(){ 
     event.preventDefault();
-    window.location.href = "https://www68.myfantasyleague.com/2021/ajax_ls?L=48963";
+    window.location.href = "https://www68.myfantasyleague.com/2022/ajax_ls?L=48963";
 });
 $("#summary2div").click(function(){     
         event.preventDefault();    
-        window.location.href = "https://www68.myfantasyleague.com/2021/ajax_ls?L=48963";
+        window.location.href = "https://www68.myfantasyleague.com/2022/ajax_ls?L=48963";
 });
